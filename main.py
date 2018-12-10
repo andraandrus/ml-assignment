a99164a3c7470e34f062637d5ae0132a5f4fdd11
axa1067 <axa1067@cs.bham.ac.uk>
2018-12-10 11:53:36 +0000

    calculate baseline model and fix regression method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 import numpy as np
 import sklearn.linear_model as lm
-from sklearn.model_selection import KFold
+from sklearn import model_selection
 from sklearn import preprocessing as pre
 from sklearn.neighbors import KNeighborsRegressor
 import random
@@ -12,49 +12,36 @@ from sklearn.preprocessing import StandardScaler
 import sklearn.metrics as metrics
 
 def regression(housing):
-    model = lm.LinearRegression()
+	model = lm.LinearRegression()
 
-    # First, extract the data into arrays
-    y = housing.median_house_value.values.reshape(-1, 1)
-    X = housing.drop(columns=['median_house_value'], inplace=False).values
+	# First, extract the data into arrays
+	y = housing.median_house_value.values.reshape(-1, 1)
+	X = housing.drop(columns=['median_house_value'], inplace=False).values
 
-    # Pull out values into a holdout set of unseen data
-    holdout = random.sample(range(0, 20639), 5000) 
-   
-    X_unseen = X[holdout]
-    y_unseen = y[holdout]
-    
-    Xt = np.delete(X, holdout, 0)
-    yt = np.delete(y, holdout, 0)
+	# Pull out values into a holdout set of unseen data
+	holdout = random.sample(range(0, 20639), 5000) 
 
-    # Have to shuffle the data because it is grouped.
-    kf = KFold(n_splits=5, shuffle=True)
+	X_unseen = X[holdout]
+	y_unseen = y[holdout]
 
-    best_model = [0, 0, 0]  # x index, y index, score   
+	Xt = np.delete(X, holdout, 0)
+	yt = np.delete(y, holdout, 0)
 
-    for train_index, test_index in kf.split(Xt):
-        X_train, X_test = Xt[train_index], Xt[test_index]
-        y_train, y_test = yt[train_index], yt[test_index]
-        model.fit(X_train, y_train)
-        accuracy = model.score(X_test, y_test)
-        if accuracy > best_model[2]:
-                best_model[2] = accuracy
-                best_model[0] = X_train
-                best_model[1] = y_train
-        print('Training accuracy: ' + str(accuracy*100) + ' %')
+	# Perform cross validation for model
+	seed = 3
+	kfold = model_selection.KFold(n_splits=5, random_state=seed, shuffle=True)
+	results = model_selection.cross_val_score(model, Xt, yt, cv=kfold, scoring='r2')
+	r2 = results.mean()*100.0
+	print("Cross Validation results:")
+	print("R2 mean: %.3f%%, Standard deviation: %.3f%%" % (r2, results.std()*100.0))
 
-    optimal_model = model.fit(best_model[0], best_model[1])
-    predicted_values = optimal_model.predict(X_unseen)
-    score = metrics.r2_score(y_unseen, predicted_values)
-
-    print('R2 score on unseen data:' + str(score*100))
-    return optimal_model
+	model_and_score = {'model': model, 'score': r2}
+	return model_and_score
 
 # Import housing data to Pandas DataFrame
 housing = pandas.read_csv('housing.csv')
 
-# ocean_proximity can have the following values 'ISLAND' 'NEAR_OCEAN' 'INLAND' '<1H OCEAN' 'NEAR BAY'
-
+# Replace ocean_proximity categorical variable with one-hot vectors
 housing['1h_ocean'] = [1 if i=='<1H OCEAN' else 0 for i in housing.ocean_proximity.values]
 housing['island'] = [1 if i=='ISLAND' else 0 for i in housing.ocean_proximity.values]
 housing['inland'] = [1 if i=='INLAND' else 0 for i in housing.ocean_proximity.values]
@@ -66,14 +53,16 @@ initialHousing = housing.copy()
 
 # total_bedrooms has missing values.
 # Approaches:
-# 1. replace with column average
 
+# 1. replace with column average
 mean = housing['total_bedrooms'].mean() 
 housing['total_bedrooms'].fillna(mean, inplace =True)
 
 print("1. replace with column average")
 print("------------------------------")
-regression(housing)
+
+col_avg_model = regression(housing)
+col_avg_model['name'] = 'replaced missing values with column average'
 
 # 2. replace with values from nearest neighbour
 housing = initialHousing.copy()
@@ -102,7 +91,8 @@ housing.loc[housing.total_bedrooms.isna(), 'total_bedrooms'] = predicted_values
 
 print("\n2. replace with values from nearest neighbour")
 print("--------------------------------")
-regression(housing)
+knn_model = regression(housing)
+knn_model['name'] = 'replaced missing values with KNN'
 
 # 3. use regression with the values in the total_rooms column as prior knowledge
 housing = initialHousing.copy()
@@ -119,7 +109,34 @@ housing.total_bedrooms.loc[isna] = np.squeeze(missing_bedrooms)
 
 print("\n3. use prior knowledge")
 print("--------------------------------")
-price_model = regression(housing)
+pk_model = regression(housing)
+pk_model['name'] = 'replaced missing values using prior knowledge'
+
+# Need to decide on the best of the three models => baseline
+model_list = [col_avg_model, knn_model, pk_model]
+best_score = max(col_avg_model['score'], knn_model['score'], pk_model['score'])
+baseline_model = None
+for model in model_list:
+	if best_score == model['score']:
+		baseline_model = model
+
+# Test baseline model on unseen data
+# Extract the data into arrays
+y = housing.median_house_value.values.reshape(-1, 1)
+X = housing.drop(columns=['median_house_value'], inplace=False).values
+
+# Pull out values into a holdout set of unseen data
+holdout = random.sample(range(0, 20639), 5000) 
+X_unseen = X[holdout]
+y_unseen = y[holdout]
+
+Xt = np.delete(X, holdout, 0)
+yt = np.delete(y, holdout, 0)
+
+baseline_model['model'].fit(Xt, yt)
+predicted_values = baseline_model['model'].predict(X_unseen)
+score = metrics.r2_score(y_unseen, predicted_values)
+print("R2 score on unseen data when " + baseline_model['name'] + ': ' + str(score*100))
 
 # Drop logically irrelevant columns - neet to normalise beforehand:
 # use regression_model.coef to figure out unimportant variables
