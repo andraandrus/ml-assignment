16a4ce458269d61f445945adb45123aa2cd2e997
axa1067 <axa1067@cs.bham.ac.uk>
2018-12-11 14:30:55 +0000

    fix knn

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def regression(housing):
 	yt = np.delete(y, holdout, 0)
 
 	# Perform cross validation for model
-	r2 = cross_validation(model, Xt, yt)
+	r2 = rrtts(model, X, y)
 
 	model_and_score = {'model': model, 'score': r2}
 	return model_and_score
@@ -88,53 +88,55 @@ initialHousing = housing.copy()
 # total_bedrooms has missing values.
 # Approaches:
 
-# 1. replace with column average
-mean = housing['total_bedrooms'].mean() 
-housing['total_bedrooms'].fillna(mean, inplace =True)
+# # 1. replace with column average
+# mean = housing['total_bedrooms'].mean() 
+# housing['total_bedrooms'].fillna(mean, inplace =True)
 
-print("1. replace with column average")
-print("------------------------------")
+# print("1. replace with column average")
+# print("------------------------------")
 
-col_avg_model = regression(housing)
-col_avg_model['name'] = 'replaced missing values with column average'
+# col_avg_model = regression(housing)
+# col_avg_model['name'] = 'replaced missing values with column average'
 
-# 2. replace with most frequent
+# # 2. replace with most frequent
 
-housing = initialHousing.copy()
+# housing = initialHousing.copy()
 
-imp = SimpleImputer(strategy="most_frequent")
-housing_array = imp.fit_transform(housing)
-columns = ['longitude', 'latitude',	'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'median_house_value',
-			'1h_ocean', 'island', 'inland', 'near_ocean', 'near_bay']
+# imp = SimpleImputer(strategy="most_frequent")
+# housing_array = imp.fit_transform(housing)
+# columns = ['longitude', 'latitude',	'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'median_house_value',
+# 			'1h_ocean', 'island', 'inland', 'near_ocean', 'near_bay']
 
-housing = pandas.DataFrame(data=housing_array[1:, 0:], columns=columns)  
+# housing = pandas.DataFrame(data=housing_array[1:, 0:], columns=columns)  
 
-print("\n2. replace with most frequent")
-print("------------------------------")
-most_frequent_model = regression(housing)
-most_frequent_model['name'] = 'replaced missing values with most frequent value'
+# print("\n2. replace with most frequent")
+# print("------------------------------")
+# most_frequent_model = regression(housing)
+# most_frequent_model['name'] = 'replaced missing values with most frequent value'
 
 # 3. replace with values from nearest neighbour
 housing = initialHousing.copy()
 
 housing_missing = housing[housing.total_bedrooms.isna()] # these we want to predict
-housing_missing = housing_missing['total_rooms']
+housing_missing = housing_missing[['longitude', 'latitude',	'housing_median_age', 'total_rooms',
+		 	'population', 'households', 'median_income', 'median_house_value',
+			'1h_ocean', 'island', 'inland', 'near_ocean', 'near_bay']]
 
 housing_not_missing = housing[housing.total_bedrooms.notna()]
-X_train = housing_not_missing['total_rooms']                   # data  (X) -> longitude, latitude, total_rooms, population, households, median_house_value
-y_train = housing_not_missing[['total_bedrooms']]              # label (y) -> total_bedrooms (column we want to predict)
 
-X_shaped = []
-for index in range(0, len(X_train)):
-        X_shaped.append([X_train.index[index]])
+X_train = housing_not_missing[['longitude', 'latitude',	'housing_median_age', 'total_rooms',
+		 	'population', 'households', 'median_income', 'median_house_value',
+			'1h_ocean', 'island', 'inland', 'near_ocean', 'near_bay']]         # data  (X) -> longitude, latitude, total_rooms, population, households, median_house_value
+
+y_train = housing_not_missing[['total_bedrooms']]            				   # label (y) -> total_bedrooms (column we want to predict)
 
 regressor = KNeighborsRegressor(n_neighbors=5)
-regressor.fit(X_shaped, y_train)
+regressor.fit(X_train, y_train)
 
 predicted_values = []
 for index in range(0, len(housing_missing)):
         values = housing_missing.iloc[index].tolist()
-        y_pred = regressor.predict([[values]])
+        y_pred = regressor.predict([values])
         predicted_values.append(y_pred[0][0])
 
 housing.loc[housing.total_bedrooms.isna(), 'total_bedrooms'] = predicted_values
@@ -144,54 +146,54 @@ print("--------------------------------")
 knn_model = regression(housing)
 knn_model['name'] = 'replaced missing values with KNN'
 
-# 4. use regression with the values in the total_rooms column as prior knowledge
-housing = initialHousing.copy()
+# # 4. use regression with the values in the total_rooms column as prior knowledge
+# housing = initialHousing.copy()
 
-notna = housing.total_bedrooms.notna()
-isna = housing.total_bedrooms.isna()
+# notna = housing.total_bedrooms.notna()
+# isna = housing.total_bedrooms.isna()
 
-model = lm.LinearRegression()
-model.fit(housing.total_rooms.values[notna].reshape(-1,1), housing.total_bedrooms.values[notna].reshape(-1,1))
-model.score(housing.total_rooms.values[notna].reshape(-1,1), housing.total_bedrooms.values[notna].reshape(-1,1))
+# model = lm.LinearRegression()
+# model.fit(housing.total_rooms.values[notna].reshape(-1,1), housing.total_bedrooms.values[notna].reshape(-1,1))
+# model.score(housing.total_rooms.values[notna].reshape(-1,1), housing.total_bedrooms.values[notna].reshape(-1,1))
 
-missing_bedrooms = model.predict(housing.total_rooms.values[isna].reshape(-1,1))
-housing.total_bedrooms.loc[isna] = np.squeeze(missing_bedrooms)
+# missing_bedrooms = model.predict(housing.total_rooms.values[isna].reshape(-1,1))
+# housing.total_bedrooms.loc[isna] = np.squeeze(missing_bedrooms)
 
-print("\n4. use prior knowledge")
-print("--------------------------------")
-pk_model = regression(housing)
-pk_model['name'] = 'replaced missing values using prior knowledge'
+# print("\n4. use prior knowledge")
+# print("--------------------------------")
+# pk_model = regression(housing)
+# pk_model['name'] = 'replaced missing values using prior knowledge'
 
 # Need to decide on the best of the three models => baseline
-model_list = [col_avg_model, knn_model, pk_model]
-best_score = max(col_avg_model['score'], knn_model['score'], pk_model['score'])
-baseline_model = None
-for model in model_list:
-	if best_score == model['score']:
-		baseline_model = model
+# model_list = [col_avg_model, knn_model, pk_model, most_frequent_model]
+# best_score = max(col_avg_model['score'], knn_model['score'], pk_model['score'])
+# baseline_model = None
+# for model in model_list:
+# 	if best_score == model['score']:
+# 		baseline_model = model
 
-# Test baseline model on unseen data
-# Extract the data into arrays
-y = housing.median_house_value.values.reshape(-1, 1)
-X = housing.drop(columns=['median_house_value'], inplace=False).values
+# # Test baseline model on unseen data
+# # Extract the data into arrays
+# y = housing.median_house_value.values.reshape(-1, 1)
+# X = housing.drop(columns=['median_house_value'], inplace=False).values
 
-# Pull out values into a holdout set of unseen data
-holdout = random.sample(range(0, 20639), 5000) 
-X_unseen = X[holdout]
-y_unseen = y[holdout]
+# # Pull out values into a holdout set of unseen data
+# holdout = random.sample(range(0, 20639), 5000) 
+# X_unseen = X[holdout]
+# y_unseen = y[holdout]
 
-Xt = np.delete(X, holdout, 0)
-yt = np.delete(y, holdout, 0)
+# Xt = np.delete(X, holdout, 0)
+# yt = np.delete(y, holdout, 0)
 
-baseline_model['model'].fit(Xt, yt)
-predicted_values = baseline_model['model'].predict(X_unseen)
-score = metrics.r2_score(y_unseen, predicted_values)
-print("R2 score on unseen data when " + baseline_model['name'] + ': ' + str(score*100))
+# baseline_model['model'].fit(Xt, yt)
+# predicted_values = baseline_model['model'].predict(X_unseen)
+# score = metrics.r2_score(y_unseen, predicted_values)
+# print("R2 score on unseen data when " + baseline_model['name'] + ': ' + str(score*100))
 
-# Drop logically irrelevant columns - neet to normalise beforehand:
-# use regression_model.coef to figure out unimportant variables
+# # Drop logically irrelevant columns - neet to normalise beforehand:
+# # use regression_model.coef to figure out unimportant variables
 
-housing = housing.drop(columns=['median_house_value'])
+# housing = housing.drop(columns=['median_house_value'])
 
 # for index, col_name in enumerate(housing.columns):
 # 	print('column: ' + col_name + ', coef:' + str(price_model.coef_[0][index-1]))
